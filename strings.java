85066ebc3415fae6da7d071a617a9e428f3516ca
SDevgon <45467719+SDevgon@users.noreply.github.com>
2019-03-24 11:24:33 +0530

    Add files via upload

---
+++ strings.java
@@ -0,0 +1,8 @@
+public class StringDemo {
+
+	   public static void main(String args[]) {
+		         String string1 = "great";
+			       System.out.println("Feeling " + string1 + "Today");
+			       int length=string.length(();
+					       System.out,println(lenth);
+	   }}
